6627bec150503a88896f015581cac9d8ca1197c5
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-03-29 18:32:47 +0300

    Create draft Viewer component with module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'viewer',
+    loadChildren: () => import('./viewer/viewer.module').then((m) => m.ViewerModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'viewer'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/viewer/viewer.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-viewer',
+  templateUrl: './viewer.component.html',
+  styleUrls: ['./viewer.component.scss'],
+})
+export class ViewerComponent {}
