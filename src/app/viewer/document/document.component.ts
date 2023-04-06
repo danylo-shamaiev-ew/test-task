f3f6ec085f74b8be9eaaea0fb86ec5650de284ab
Danylo Shamaiev <danyloshamaiev@gmail.com>
2023-04-06 17:39:22 +0300

    Fix annotation dragging

--- src/app/viewer/document/document.component.ts
+++ src/app/viewer/document/document.component.ts
@@ -1,12 +1,14 @@
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef,
-  Component, EventEmitter,
-  Input, Output
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
 } from '@angular/core';
 import {Document} from '../../core/models/document.model';
 import {Annotation} from '../../core/models/annotation.model';
-import {DocumentPage} from '../../core/models/document-page.model';
-import {fromEvent, takeUntil, tap} from 'rxjs';
+import {fromEvent, map, takeUntil, tap} from 'rxjs';
 
 @Component({
   selector: 'app-document',
@@ -20,7 +22,7 @@ export class DocumentComponent {
   public newAnnotationText = '';
   public newAnnotationImageUrl = '';
 
-  constructor(private readonly cdRef: ChangeDetectorRef) {}
+  // constructor(private readonly cdRef: ChangeDetectorRef) {}
 
   public handleAddAnnotation($event: MouseEvent, pageIndex: number) {
     $event.stopPropagation();
@@ -37,7 +39,7 @@ export class DocumentComponent {
     this.document = newDocument;
   }
 
-  public handleSaveAnnotationAsText(pageIndex:number, annotationIndex: number) {
+  public handleSaveAnnotationAsText(pageIndex: number, annotationIndex: number) {
     const newDocument = {...this.document};
     // @ts-ignore
     newDocument.pages[pageIndex].annotations[annotationIndex].type = 'text';
@@ -47,7 +49,7 @@ export class DocumentComponent {
     this.documentChanged.next(newDocument);
   }
 
-  public handleSaveAnnotationAsImage(pageIndex:number, annotationIndex: number) {
+  public handleSaveAnnotationAsImage(pageIndex: number, annotationIndex: number) {
     const newDocument = {...this.document};
     // @ts-ignore
     newDocument.pages[pageIndex].annotations[annotationIndex].type = 'image';
@@ -61,7 +63,6 @@ export class DocumentComponent {
     $event.stopPropagation();
     const newDocument = {...this.document};
     newDocument.pages[pageIndex].annotations?.splice(annotationIndex, 1);
-    // this.document = newDocument;
     this.documentChanged.next(newDocument);
   }
 
@@ -71,46 +72,41 @@ export class DocumentComponent {
 
   public handleAnnotationDrag($event: Event, pageIndex: number, annotationIndex: number) {
     $event.stopPropagation();
-    fromEvent(document, 'pointermove')
-      .pipe(takeUntil(fromEvent(document, 'mouseup').pipe(tap(()=>{
-        this.cdRef.detectChanges();
-      }))))
-      .subscribe((event)=> {
-      const newDocument = {...this.document};
-      // @ts-ignore
-      newDocument.pages[pageIndex].annotations[annotationIndex].coordinates = [event.offsetX, event.offsetY];
-      // this.document = newDocument;
-      this.documentChanged.next(newDocument);
-    })
+    const pageElement = document.getElementById(`page-${pageIndex}`) as HTMLElement;
+    const pageRect = pageElement.getBoundingClientRect();
+    const annotationElement = pageElement.getElementsByClassName('annotation')[
+      annotationIndex
+    ] as HTMLElement;
+    fromEvent(document, 'mousemove')
+      .pipe(
+        takeUntil(
+          fromEvent(document, 'mouseup').pipe(
+            tap(() => {
+              const newDocument = {...this.document};
+              if (!newDocument.pages[pageIndex].annotations) return;
+              const pageAnnotations = newDocument.pages[pageIndex].annotations as Annotation[];
+              pageAnnotations[annotationIndex].coordinates = [
+                parseInt(annotationElement.style.left, 10),
+                parseInt(annotationElement.style.top, 10),
+              ];
+              this.documentChanged.next(newDocument);
+            })
+          )
+        ),
+        map((e) => e as MouseEvent)
+      )
+      .subscribe((event) => {
+        annotationElement.style.left = `${event.clientX - pageRect.x}px`;
+        annotationElement.style.top = `${event.clientY - pageRect.y}px`;
+      });
   }
 
-  public calculateAnnotationNgStyle(
-    page: DocumentPage,
-    annotation: Annotation,
-    annotationIndex: number
-  ) {
-    // @ts-ignore
-    const annotationsCorrection = page.annotations.reduce(
-      (accumulator, curAnnotation, curIndex, arr) => {
-        if (curIndex >= annotationIndex) return accumulator;
-        return accumulator + curAnnotation.size[0] + 16;
-      },
-      0
-    );
+  public calculateAnnotationNgStyle(annotation: Annotation) {
     return {
-      top: `${annotation.coordinates[1] - annotationsCorrection}px`,
+      top: `${annotation.coordinates[1]}px`,
       left: `${annotation.coordinates[0]}px`,
       height: `${annotation.size[0]}px`,
       width: `${annotation.size[1]}px`,
     };
-  }
-
-  public calculateNegativeMargin(page: DocumentPage) {
-    const paddingCorrection = 16;
-    const totalHeight = page?.annotations?.reduce(
-      (accumulator, curAnnotation) => accumulator + (curAnnotation.size[0] + paddingCorrection),
-      0
-    );
-    return {'margin-top': `-${totalHeight}px`};
   }
 }
